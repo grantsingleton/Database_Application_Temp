f68b86afce22b9d0c700a5e8acaa2a0e0bd518d0
grantsingleton <46718281+grantsingleton@users.noreply.github.com>
2019-03-15 14:50:11 -0500

    Add files via upload

---
+++ Functions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Table.h"
+#include "Record.h"
+#include "Database.h"
+#include "Query.h"
+#include "rapidjson.h"  // for json parsing
+#include "document.h"
+#include "stringbuffer.h"
+
+
+Database* populateDatabase();
